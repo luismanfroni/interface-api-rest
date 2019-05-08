80b589d3313a41b2a116b266f94ff02e3ba8421a
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2019-05-08 18:29:25 -0300

    Builded

---
+++ build/precache-manifest.764bdf1ca1b1a97844336717c49fe424.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fabae31eb077768646a",
+    "url": "/static/js/main.f363061d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "120486d5ea3d110ed94e",
+    "url": "/static/js/2.a45a557d.chunk.js"
+  },
+  {
+    "revision": "f24d961ff73a3edd31ca1607500213d5",
+    "url": "/index.html"
+  }
+];
